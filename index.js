2ff7c502b0fe32dcee63936cf24a0a2823c0e0e1
liz <rhodes.liz@gmail.com>
2022-06-05 20:22:56 -0400

    commit tree 45aa38afa459ea1ecb65bfe2b4f5f22ddb297db5

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+      let  messages = [];
+    for (let i = 0; i < names.length; i++) {
+       messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return messages;
+}
+
+function countDown(startingNumber) {
+    //let count = 10
+ while (startingNumber >= 0) {
+    console.log(startingNumber);
+    startingNumber --;
+       
+} 
+
+}
